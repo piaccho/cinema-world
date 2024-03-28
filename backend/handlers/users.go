c78ba2aeecb5ab7334e0472f81476f6800a03365
piaccho <sebi77styp@gmail.com>
2024-03-28 20:24:03 +0100

    Initalize Users handlerin BE

--- backend/handlers/users.go
+++ backend/handlers/users.go
@@ -1,18 +1,78 @@
 package handlers
 
 import (
+	"context"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 )
+
+var validate = validator.New()
 
 func LoginUser(client *mongo.Client) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var user User
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
 
+		err := validate.Struct(user)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		collection := client.Database("test").Collection("users")
+		var result User
+		err = collection.FindOne(context.TODO(), bson.M{"email": user.Email}).Decode(&result)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid email or password"})
+			return
+		}
+
+		err = bcrypt.CompareHashAndPassword([]byte(result.Password), []byte(user.Password))
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid email or password"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"message": "Logged in successfully"})
 	}
 }
 
 func RegisterUser(client *mongo.Client) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var user User
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
 
+		err := validate.Struct(user)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while hashing the password"})
+			return
+		}
+
+		user.Password = string(hash)
+		collection := client.Database("test").Collection("users")
+		_, err = collection.InsertOne(context.TODO(), user)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while inserting the user"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"message": "Registered successfully"})
 	}
 }
