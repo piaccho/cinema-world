81c3a26d10c9a0a7d29bdeb08c2ff3f63802bbc9
piaccho <sebi77styp@gmail.com>
2024-03-28 21:18:49 +0100

    Update BE Dockerfile

--- backend/handlers/users.go
+++ backend/handlers/users.go
@@ -1,78 +1,67 @@
 package handlers
 
-import (
-	"context"
-	"net/http"
+// var validate = validator.New()
 
-	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"golang.org/x/crypto/bcrypt"
-)
+// func LoginUser(client *mongo.Client) gin.HandlerFunc {
+// 	return func(c *gin.Context) {
+// 		var user User
+// 		if err := c.ShouldBindJSON(&user); err != nil {
+// 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 			return
+// 		}
 
-var validate = validator.New()
+// 		err := validate.Struct(user)
+// 		if err != nil {
+// 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 			return
+// 		}
 
-func LoginUser(client *mongo.Client) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		var user User
-		if err := c.ShouldBindJSON(&user); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
+// 		collection := client.Database("test").Collection("users")
+// 		var result User
+// 		err = collection.FindOne(context.TODO(), bson.M{"email": user.Email}).Decode(&result)
+// 		if err != nil {
+// 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid email or password"})
+// 			return
+// 		}
 
-		err := validate.Struct(user)
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
+// 		err = bcrypt.CompareHashAndPassword([]byte(result.Password), []byte(user.Password))
+// 		if err != nil {
+// 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid email or password"})
+// 			return
+// 		}
 
-		collection := client.Database("test").Collection("users")
-		var result User
-		err = collection.FindOne(context.TODO(), bson.M{"email": user.Email}).Decode(&result)
-		if err != nil {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid email or password"})
-			return
-		}
+// 		c.JSON(http.StatusOK, gin.H{"message": "Logged in successfully"})
+// 	}
+// }
 
-		err = bcrypt.CompareHashAndPassword([]byte(result.Password), []byte(user.Password))
-		if err != nil {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid email or password"})
-			return
-		}
+// func RegisterUser(client *mongo.Client) gin.HandlerFunc {
+// 	return func(c *gin.Context) {
+// 		var user User
+// 		if err := c.ShouldBindJSON(&user); err != nil {
+// 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 			return
+// 		}
 
-		c.JSON(http.StatusOK, gin.H{"message": "Logged in successfully"})
-	}
-}
+// 		err := validate.Struct(user)
+// 		if err != nil {
+// 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 			return
+// 		}
 
-func RegisterUser(client *mongo.Client) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		var user User
-		if err := c.ShouldBindJSON(&user); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
+// 		hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+// 		if err != nil {
+// 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while hashing the password"})
+// 			return
+// 		}
 
-		err := validate.Struct(user)
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
+// 		user.Password = string(hash)
+// 		collection := client.Database("test").Collection("users")
+// 		_, err = collection.InsertOne(context.TODO(), user)
+// 		if err != nil {
+// 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while inserting the user"})
+// 			return
+// 		}
 
-		hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while hashing the password"})
-			return
-		}
-
-		user.Password = string(hash)
-		collection := client.Database("test").Collection("users")
-		_, err = collection.InsertOne(context.TODO(), user)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while inserting the user"})
-			return
-		}
-
-		c.JSON(http.StatusOK, gin.H{"message": "Registered successfully"})
-	}
-}
+// 		c.JSON(http.StatusOK, gin.H{"message": "Registered successfully"})
+// 	}
+// }
--- backend/routes/users.go
+++ backend/routes/users.go
@@ -1,13 +1,6 @@
 package routes
 
-import (
-	"piaccho/cinema-api/handlers"
-
-	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/mongo"
-)
-
-func SetupUsersRoutes(router *gin.RouterGroup, client *mongo.Client) {
-	router.POST("/login", handlers.LoginUser(client))
-	router.POST("/register", handlers.RegisterUser(client))
-}
+// func SetupUsersRoutes(router *gin.RouterGroup, client *mongo.Client) {
+// 	router.POST("/login", handlers.LoginUser(client))
+// 	router.POST("/register", handlers.RegisterUser(client))
+// }
